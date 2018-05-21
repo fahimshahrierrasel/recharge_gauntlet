f534991d9be7f48ff481fc4ae925cd16cb87e475
Unknown <ahashans@gmail.com>
2018-05-21 12:42:17 +0600

    Added Recharge Form

---
+++ RechargeGauntlet/RechargeMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RechargeGauntlet
+{
+    public partial class RechargeMain : Form
+    {
+        public RechargeMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
