1ad556108b583cb823ac7c46f7c0e864ac72ac8d
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-05-24 19:50:42 +0600

    modem port and operator name get class add.

---
+++ RechargeGauntlet/Modem/OperatorModemPort.cs
@@ -0,0 +1,13 @@
+﻿namespace RechargeGauntlet.Modem
+{
+    public struct OperatorModemPort
+    {
+        public string OperatorName, ComPort;
+
+        public OperatorModemPort(string operatorName, string comPort)
+        {
+            OperatorName = operatorName;
+            ComPort = comPort;
+        }
+    }
+}
